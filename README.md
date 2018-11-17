f17e59b0c2ebf0cc18025221ed3c1e2f4eb2ffb3
Sriram Kaushik <skaushik@box.com>
2018-11-16 17:49:23 -0800

    v1

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # googlespeech2text
 google speech to text sample
+
+Run SOX in mac to convert to Linear16
+
+//Raw local audio file in "wav" format
+mbp-008814:GCP_AI sk$ ls -lrt sk1a.wav 
+-rw-r--r--@ 1 sk  AD\Domain Users  1025792 Nov 16 14:17 sk1a.wav
+mbp-008814:GCP_AI sk$
+
+//Convert to LINEAR16
+mbp-008814:GCP_AI sk$ sox sk1a.wav --channels=1 --rate 16k --bits 16 sk1a.raw
+
+mbp-008814:GCP_AI sk$ ls -lrt sk1a.raw 
+-rw-r--r--  1 sk  Users  1021696 Nov 16 14:17 sk1a.raw
+mbp-008814:GCP_AI sk$ 
+
+//Result from the speech below: Sample code attached. 
+mbp-008814:GCP_AI sk$ ./speechtotext -input sk1a.raw 
+"four score and seven years ago our fathers brought forth on this continent a new nation conceived in liberty and dedicated to the proposition that all men are created equal now we are engaged in a great civil war testing whether that Nation or any Nation so conceived and so dedicated can long endure we are met on a great battlefield of that war we have come to dedicate a portion of that field as a final resting place for those who here gave their lives that that Nation might live it is also it is altogether fitting and proper that we should do this" (confidence=0.979804)
+mbp-008814:GCP_AI sk$
---
+++ speechtotext.go
@@ -0,0 +1,67 @@
+//Speech to Text testing with a service account
+
+package main
+
+import (
+	speech "cloud.google.com/go/speech/apiv1"
+	"context"
+	"flag"
+	"fmt"
+	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+
+	//get the input file.
+
+	var speech_file *string = flag.String("input", "/Users/skaushik/Documents/my_dev_env/go_workspace/src/github.com/kaushiksriram100/GCP_AI/somespeech.raw", "the input *.raw file. To convert - sox bob1.wav --channels=1 --rate 16k --bits 16 bob1a.raw")
+	flag.Parse()
+	//creates a context that never expires
+	ctx := context.Background()
+
+	//create a client with the above context
+	client, err := speech.NewClient(ctx)
+
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+
+	// Set the name of the audio file to transcribe
+
+	//Read the audio file into the memory. ReadFile return []bytes (go style!)
+	data, err := ioutil.ReadFile(*speech_file)
+
+	if err != nil {
+		log.Fatalf("failed to read audio file. May be corrupted. %v", err)
+	}
+
+	//Create a speech config
+
+	speech_conf := &speechpb.RecognizeRequest{
+		Config: &speechpb.RecognitionConfig{
+			Encoding:        speechpb.RecognitionConfig_LINEAR16,
+			SampleRateHertz: 16000,
+			LanguageCode:    "en-US",
+		},
+		Audio: &speechpb.RecognitionAudio{
+			AudioSource: &speechpb.RecognitionAudio_Content{Content: data},
+		},
+	}
+	//detect speech portions of the audio file
+	resp, err := client.Recognize(ctx, speech_conf)
+
+	if err != nil {
+		log.Fatalf("failed to recognize: %v", err)
+
+	}
+
+	for _, result := range resp.Results {
+		for _, alt := range result.Alternatives {
+			fmt.Printf("\"%v\" (confidence=%3f)\n", alt.Transcript, alt.Confidence)
+		}
+	}
+	return
+
+}
